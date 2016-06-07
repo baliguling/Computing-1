79c3d7c6696d44ef755ed7c13d100959e913e74b
baliguling <baliguling@hotmail.com>
2016-06-07 22:41:21 +1000

    Update frequencyanalysis.py

--- frequencyanalysis.py
+++ frequencyanalysis.py
@@ -1,7 +1,8 @@
 from string import ascii_lowercase
+from string import ascii_uppercase
 from collections import Counter
 
 with open('text.txt') as f:
     print Counter(letter for line in f 
                   for letter in line.lower() 
-                  if letter in ascii_lowercase)
+                  if letter in ascii_lowercase, ascii_uppercase)
